dd34bea81e6fe9d1fcfb2735a0f47db33efd11b7
Tsumir <tsumerovanton@gmail.com>
2024-07-31 18:03:03 +0300

    app.js Проверка git прошла успешно

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = config.get('port') || 5000
-const PORT = config.get('port') || 5000
+
 async function start() {
     try{
         await mongoose.connect(config.get('mongoUri'), {
